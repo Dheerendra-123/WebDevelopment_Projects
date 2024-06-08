847033132c9ff2d9777557fe297c972b4cf3ce35
Dheerendra Vikram Dixit <129359818+Dheerendra-123@users.noreply.github.com>
2024-06-08 18:31:10 +0530

    Add files via upload

---
+++ YouTube Clone/script.js
@@ -0,0 +1,8 @@
+var menuIcon=document.querySelector(".menu.icon");
+var sidebar=document.querySelector(".sidebar");
+var container=document.querySelector(".container")
+
+function Toggle(){
+    sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container")   
+    }
